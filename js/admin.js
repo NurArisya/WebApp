46c08e4e0afd1ed13cdacc98e932b056de048d05
Ayu Farisha <108130667+farishaa@users.noreply.github.com>
2025-05-27 14:46:28 +0800

    admin.js

--- js/admin.js
+++ js/admin.js
@@ -2,27 +2,34 @@ const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let intervalID = null;
 
-//initializeSlider();
-document.addEventListener("DOMContentLoaded", initializeSlider);
-
-function initializeSlider() {
+document.addEventListener("DOMContentLoaded", function () {
     if (slides.length > 0) {
         slides[slideIndex].classList.add("displaySlide");
         intervalID = setInterval(nextSlide, 5000);
     }
-}
+
+    const logoutBtn = document.querySelector('.logout-btn');
+    if (logoutBtn) {
+        logoutBtn.addEventListener('click', function () {
+            // fetch('/logout', { method: 'POST' });
+
+            window.location.href = 'login.html';
+        });
+
+    }
+});
 
 function showSlide(index) {
-    if(index >= slides.length){
+    if (index >= slides.length) {
         slideIndex = 0;
-    }
-    else if(index < 0){
-        slideIndex = slides.length -1;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
     }
 
     slides.forEach(slide => {
         slide.classList.remove("displaySlide");
     });
+
     slides[slideIndex].classList.add("displaySlide");
 }
 
