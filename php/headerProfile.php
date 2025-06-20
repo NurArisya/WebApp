f30cb47d46240863226c0db8163ef79462a4b7c6
NurArisya <124141390+NurArisya@users.noreply.github.com>
2025-06-20 20:48:16 +0800

    Add files via upload

---
+++ php/headerProfile.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="../css/dashboard.css" />
+
+<header>
+<nav>
+    <div class="navheader">
+        <div class="navlogo">
+            <a href="#">MMU<span>Spotlit</span></a>
+        </div>
+
+        <ul class="navlinks" id="nav-links">
+            <li><a href="./dashboard.php">Back to Homepage</a></li>
+                <a href="./about.php"></a>
+                <ul class="dropdown-menu">
+            </li>
+        </ul>
+    </div>
+</nav>
+</header>
