4b844cc6a912bf844592bd5b2cedfbb9aa1a3935
Ayu Farisha <108130667+farishaa@users.noreply.github.com>
2025-06-22 22:11:23 +0800

    Update community.php

--- php/community.php
+++ php/community.php
@@ -2,6 +2,13 @@
 include 'db_conn.php';
 session_start();
 
+$loggedIn = isset($_SESSION['userID']);
+$profileLink = '../php/talentProfile.php';
+
+if (isset($_SESSION['role']) && $_SESSION['role'] === 'customer') {
+    $profileLink = '../php/custProfile.php';
+}
+
 $userID = $_SESSION['userID'] ?? null;
 
 // Handlee discussion submission
@@ -55,10 +62,59 @@ $result = $conn->query($sql);
 <head>
     <title>Community Discussion</title>
     <link rel="stylesheet" href="../css/community.css">
+    <link rel="stylesheet" href="../css/dashboard.css" />
+
     <link rel="icon" type="image/png" href="../image/spotlit-tab-logo.png">
 </head>
 <body>
-    <?php include 'header.php'; ?>
+
+<nav>
+        <div class="navheader">
+            <div class="navlogo">
+                <a href="#">MMU<span>Spotlit</span></a>
+            </div>
+
+            <ul class="navlinks" id="nav-links">
+                <li><a href="../php/dashboard.php">Home</a></li>
+                <li><a href="../php/talent.php">Talent</a></li>
+                <!--<li><a href="#">Events</a></li> -->
+                <li><a href="../php/gallery.php">Gallery</a></li>
+                <li><a href="../php/community.php">Community Services</a></li>
+                <li class="dropdown">
+                    <a href="../php/about.php">About Us</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="../php/faq.php">FAQ</a></li>
+                        <li><a href="../php/contact.php">Contact Us</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <?php if ($loggedIn): ?>
+        <!-- Logged-in User Dropdown -->
+        <ul class="navlinks">
+          <li class="dropdown">
+            <a href="#" class="dropbtn">Menu ▼</a>
+            <ul class="dropdown-menu">
+            <li><a href="<?= $profileLink ?>">My Profile</a></li>
+
+
+        <?php if (isset($_SESSION['role']) && $_SESSION['role'] !== 'customer'): ?>
+          <!-- Only show Media if not a customer -->
+          <li><a href="../php/media.php">Media</a></li>
+        <?php endif; ?>
+
+              <li><a href="../php/logout.php">Log Out</a></li>
+            </ul>
+          </li>
+        </ul>
+      <?php else: ?>
+        <!-- Guest User -->
+        <a href="../src/register.php"><button type="button" class="loginbtn">Log in</button></a>
+      <?php endif; ?>
+      </div>
+      </nav>
+
+
     <h2>Discussion</h2></br>
         <?php if ($userID): ?>
             <div class="new-discussion-form">
@@ -71,7 +127,7 @@ $result = $conn->query($sql);
                 </form>
             </div>
         <?php else: ?>
-            <p style="text-align:center;"><em>Please <a href="login.php">log in</a> to start a new discussion.</em></p>
+            <p style="text-align:center;"><em>Please <a href="../src/login.php">log in</a> to start a new discussion.</em></p>
         <?php endif; ?></br>
 
     <div class="discussion-wrapper">
