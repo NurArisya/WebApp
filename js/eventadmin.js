c7d6dcca96ffdeb887e08c1f69d4c14cc0449d0f
NurArisya <124141390+NurArisya@users.noreply.github.com>
2025-06-22 19:40:09 +0800

    Add files via upload

--- js/eventadmin.js
+++ js/eventadmin.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const logoutBtn = document.querySelector('.logout-btn');
     if (logoutBtn) {
         logoutBtn.addEventListener('click', function () {
-            window.location.href = '../php/dashboard.php';
+            window.location.href = '../dashboard.html';
         });
     }
 
